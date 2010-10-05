8d9a27566dccc80db3c49b5d8a1860dd0923e554
wilfriedvanasten <w.g.g.vanasten@student.utwente.nl>
2010-10-05 11:51:18 +0200

    Do some more work on the dummy simulator.
    There are obviously a lot of errors and missing things still
    to be resolved but at least this gives an idea of where
    we may want to go.
    
    It expects instructions to be a list of instruction lines
    (this is not properly implemented yet). This gives
    a lot of clutter but generating things to work on a list
    of opcodes makes the specification VERY complicated.

--- vm/dummy_simulator.c
+++ vm/dummy_simulator.c
@@ -1,4 +1,7 @@
 #include "simulator.h"
+#include "string.h"
+
+#define NUM_INSTR 4
 
 int nbits_cpu=8;
 char *register_name[3] = {"F","r1","r2"};
@@ -7,6 +10,46 @@ char *pin_names[0] = {};
 int npins = 0;
 size_t ramsize = 256;
 
-void instruction_handler(VMState *state, VMStateDiff *diff){
+char *instruction_name[NUM_INSTR] = {"ld","str","add","sub"};
+typedef void (*instruction_func)(VMState *state, VMStateDiff *diff, char* args);
+// LD
+void ld(VMState * state, VMStateDiff *diff, char *args){
+		
+}
+// STR
+void str(VMState * state, VMStateDiff *diff, char *args){
 	
+}
+// ADD
+void add(VMState * state, VMStateDiff *diff, char *args){
+	
+}
+// SUB
+void sub(VMState * state, VMStateDiff *diff, char *args){
+	
+}
+
+instruction_func instruction_funcs[NUM_INSTR] = {
+	ld,
+	str,
+	add,
+	sub
+};
+
+void instruction_handler(VMState *state, VMStateDiff *diff){
+	// FIXME: Are wide chars better?
+	char* instruction = (char *) state->current_instruction;
+	// FIXME: This may be difficult to generate?
+	char* space = (char *) strchr(instruction, ' ');
+	char* args = space+1;
+	// TODO: We need to find a piece of code to fit 
+	// with instruction This works but is slow
+	int i = 0;
+	while(i<NUM_INSTR){
+		if(strcmp(instruction, instruction_name[i]) == 0){
+			instruction_funcs[i](state,diff,args);
+			i=NUM_INSTR;
+		}
+		i++;
+	}
 }
