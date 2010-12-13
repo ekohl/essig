7218044c49a9268c48a19cff1942f8c1f15524f5
Ruud Harmsen <ruudharmsen1@gmail.com>
2010-12-13 11:07:29 +0100

    grammer: fixed opcode (stdyplus) and stringtemplate added quickfix

--- code-samples/factorial.c
+++ code-samples/factorial.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	char i = 10;
+	char i = 5;
 	int result = 1;
 
 	while(i>1) {
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
 		i--;
 	}
 
-	printf("%d\n", result);
+	//printf("%d\n", result);
 
-	return 0;
+	return result;
 }
