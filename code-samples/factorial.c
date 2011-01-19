b3b33f6d889187412e80c9d08957d0d55de9be7c
Mark Florisson <markflorisson88@gmail.com>
2011-01-19 21:56:47 +0100

    Future uses poster

--- code-samples/factorial.c
+++ code-samples/factorial.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
-#include <avr/io.h>
 
 #ifdef WITH_PRINTF
+#include <avr/io.h>
 static int factorial_putchar(char c, FILE *stream);
 
 static FILE _destfile = FDEV_SETUP_STREAM(factorial_putchar, NULL,
-                                          _FDEV_SETUP_WRITE);
+		                                  _FDEV_SETUP_WRITE);
 
 FILE *destfile = &_destfile;
 
 static int
 factorial_putchar(char c, FILE *stream)
 {
-    PORTA = c;
-    return 0;
+	PORTA = c;
+	return 0;
 }
 #endif /* WITH_PRINTF */
-
-void func(void) { PORTA = 2; }
 
 int main(int argc, char* argv[]) {
 	char i = 12, *resultp;
 	long result = 1;
-   
 
 	while (i > 1) {
 		result *= i;
 		i--;
 	}
-    
-    char buf[sizeof(result) + 2];
+
 #ifdef WITH_PRINTF
-    sprintf(buf, "%l\n", result);
-    fprintf(destfile, "%l\n", result);
-    fputs("foabcd", destfile);
+	char buf[sizeof(result) + 2];
+	fprintf(destfile, "%ld\n", result);
 #else
-    resultp = (char *) &result;
-    for (i = 0; i < sizeof(result); i++) {
-        PORTA = resultp[sizeof(result) - i - 1];
-    }
+	resultp = (char *) &result;
+	for (i = 0; i < sizeof(result); i++) {
+		PORTA = resultp[sizeof(result) - i - 1];
+	}
 #endif
 	return 0;
 }
